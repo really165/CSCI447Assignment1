0bc905b46d98d4c512ca136bd3eb84baaf1e20f5
really165 <31826004+really165@users.noreply.github.com>
2019-09-03 19:46:18 -0600

    HouseVotes data imputation

--- CSCI447/src/HouseVotes.java
+++ CSCI447/src/HouseVotes.java
@@ -1,3 +1,5 @@
+package project1;
+
 public class HouseVotes {
 	private String className;
 	private String handicappedInfants;
@@ -17,27 +19,105 @@ public class HouseVotes {
 	private String dutyFreeExports;
 	private String exportAdministrationActSouthAfrica;
 	
+	String[] dataArray = new String[17];
+	
 	HouseVotes(String cn, String hi, String wpcs, String aotbr, String pff, String esa, String rgis, String astb, String atnc, String mm, String i, String scc, String es, String srts, String c, String dfe, String eaasa){
-		this.className = cn;
-		this.handicappedInfants = hi;
-		this.waterProjectCostSharing = wpcs;
-		this.adoptionOfTheBudgetResolution = aotbr;
-		this.physicianFeeFreeze = pff;
-		this.elSalvadorAid = esa;
-		this.religiousGroupsInSchools = rgis;
-		this.antiSatelliteTestBan = astb;
-		this.aidToNicaraguanContras = atnc;
-		this.mxMissile = mm;
-		this.immigration = i;
-		this.synfuelsCorporationCutback = scc;
-		this.educationSpending = es;
-		this.superfundRightToSue = srts;
-		this.crime = c;
-		this.dutyFreeExports = dfe;
-		this.exportAdministrationActSouthAfrica = eaasa;
+		this.className = dataArray[0] = cn;
+		this.handicappedInfants = dataArray[1] = hi;
+		this.waterProjectCostSharing = dataArray[2] = wpcs;
+		this.adoptionOfTheBudgetResolution = dataArray[3] = aotbr;
+		this.physicianFeeFreeze = dataArray[4] = pff;
+		this.elSalvadorAid = dataArray[5] = esa;
+		this.religiousGroupsInSchools = dataArray[6] = rgis;
+		this.antiSatelliteTestBan = dataArray[7] = astb;
+		this.aidToNicaraguanContras = dataArray[8] = atnc;
+		this.mxMissile = dataArray[9] = mm;
+		this.immigration = dataArray[10] = i;
+		this.synfuelsCorporationCutback = dataArray[11] = scc;
+		this.educationSpending = dataArray[12] = es;
+		this.superfundRightToSue = dataArray[13] = srts;
+		this.crime = dataArray[14] = c;
+		this.dutyFreeExports = dataArray[15] = dfe;
+		this.exportAdministrationActSouthAfrica = dataArray[16] = eaasa;
 	}
 	
 	public void printHouseVotes(){
     	System.out.println("Class name: " + this.className + ", Handicapped infants: " + this.handicappedInfants + ", Water project cost sharing: " + this.waterProjectCostSharing + ", Adoption of the budget resolution: " + this.adoptionOfTheBudgetResolution + ", Physician fee freeze: " + this.physicianFeeFreeze + ", El salvador aid: " + this.elSalvadorAid + ", Religious groups in schools: " + this.religiousGroupsInSchools + ", Anti satellite test ban: " + this.antiSatelliteTestBan + ", Aid to nicaraguan contras: " + this.aidToNicaraguanContras + ", Mx missile: " + this.mxMissile + ", Immigration: " + this.immigration + ", Synfuels corporation cutback: " + this.synfuelsCorporationCutback + ", Education spending: " + this.educationSpending + ", Superfund right to sue: " + this.superfundRightToSue + ", Crime: " + this.crime + ", Duty free exports: " + this.dutyFreeExports + ", Export administration act south africa: " + this.exportAdministrationActSouthAfrica ); 
     }
+	
+	//updates value at given index
+	public void changeValueAtIndex(int index, String value) {
+		switch(index){
+		case 0:
+			this.className = value;
+			dataArray[0] = value;
+			break;
+		case 1:
+			this.handicappedInfants = value;
+			this.dataArray[1] = value;
+			break;
+		case 2:
+			this.waterProjectCostSharing = value;
+			this.dataArray[2] = value;
+			break;
+		case 3:
+			this.adoptionOfTheBudgetResolution = value;
+			this.dataArray[3] = value;
+			break;
+		case 4:
+			this.physicianFeeFreeze = value;
+			this.dataArray[4] = value;
+			break;
+		case 5:
+			this.elSalvadorAid = value;
+			this.dataArray[5] = value;
+			break;
+		case 6:
+			this.religiousGroupsInSchools = value;
+			this.dataArray[6] = value;
+			break;
+		case 7:
+			this.antiSatelliteTestBan = value;
+			this.dataArray[7] = value;
+			break;
+		case 8:
+			this.aidToNicaraguanContras = value;
+			this.dataArray[8] = value;
+			break;
+		case 9:
+			this.mxMissile = value;
+			this.dataArray[9] = value;
+			break;
+		case 10:
+			this.immigration = value;
+			this.dataArray[10] = value;
+			break;
+		case 11:
+			this.synfuelsCorporationCutback = value;
+			this.dataArray[11] = value;
+			break;
+		case 12:
+			this.educationSpending = value;
+			this.dataArray[12] = value;
+			break;
+		case 13:
+			this.superfundRightToSue = value;
+			this.dataArray[13] = value;
+			break;
+		case 14:
+			this.crime = value;
+			this.dataArray[14] = value;
+			break;
+		case 15:
+			this.dutyFreeExports = value;
+			this.dataArray[15] = value;
+			break;
+		case 16:
+			this.exportAdministrationActSouthAfrica = value;
+			this.dataArray[16] = value;
+			break;
+		default:
+			break;
+		}
+	}
 }
