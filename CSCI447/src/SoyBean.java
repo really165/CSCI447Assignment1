13402c7b07c480ea8050641fbc367a46dd52720f
davidr1702 <16690728+davidr1702@users.noreply.github.com>
2019-09-10 18:44:14 -0600

    Soy Bean: algorithm implemented

--- CSCI447/src/SoyBean.java
+++ CSCI447/src/SoyBean.java
@@ -36,11 +36,12 @@ public class SoyBean {
 	private int seedSize;
 	private int shriveling;
 	private int roots;
+	private String data;
 	
 	SoyBean(int date, int plantStand, int precip, int temp, int hail, int cropHist, int areaDamage, int severity, int seedTmt, int germination, 
 			int plantGrowth, int leaves, int leafspotsHalo, int leafspotsMarg, int leafspotsSize, int leafShread, int leafMalf, int leafMild, 
 			int stem, int lodging, int stemCankers, int cankerLesion, int fruitingBodies, int externalDecay, int mycelium, int intDiscolor, int sclerotia, 
-			int fruitPods,int fruitSpots, int seed, int moldGrowth, int seedDiscolor, int seedSize, int shriveling, int roots){
+			int fruitPods,int fruitSpots, int seed, int moldGrowth, int seedDiscolor, int seedSize, int shriveling, int roots, String data){
 		this.date	=	date; 
 		this.plantStand	=	plantStand;
 		this.precip	=	precip;
@@ -76,6 +77,7 @@ public class SoyBean {
 		this.seedSize	=	seedSize;
 		this.shriveling	=	shriveling;
 		this.roots	=	roots;
+		this.data	=	data;
 	}
 	
 	public void printSoyBean(){
@@ -88,6 +90,60 @@ public class SoyBean {
 		", Fruiting Bodies: "	+	this.fruitingBodies	+	", External Decay: "	+	this.externalDecay	+	", Mycelium: "	+	this.mycelium	+
 		", Int Discolor: "	+	this.intDiscolor	+	", Sclerotia: "	+	this.sclerotia	+	", Fruit Pods: "	+	this.fruitPods	+
 		", Fruit Spots: "	+	this.fruitSpots	+	", Seed: "	+	this.seed	+	", Mold Growth: "	+	this.moldGrowth	+	", Seed Discolor: "	+	this.seedDiscolor	+
-		", Seed Size: "	+	this.seedSize	+	", Shriveling: "	+	this.shriveling	+	", Roots: "	+	this.roots); 
+		", Seed Size: "	+	this.seedSize	+	", Shriveling: "	+	this.shriveling	+	", Roots: "	+	this.roots	+	", Data: "	+	this.data); 
+    }
+	
+	public int getColumn(int column) {
+    	switch(column) {
+    	case 0: return this.date;
+    	case 1: return this.plantStand;
+    	case 2: return this.precip; 
+    	case 3: return this.temp;
+    	case 4:	return this.hail; 
+    	case 5: return this.cropHist; 
+    	case 6: return this.areaDamage; 
+    	case 7: return this.severity; 
+    	case 8: return this.seedTmt; 
+    	case 9: return this.germination; 
+    	case 10: return this.plantGrowth;
+    	case 11: return this.leaves;
+    	case 12: return this.leafspotsHalo;
+    	case 13: return this.leafspotsMarg; 
+    	case 14: return this.leafspotsSize;
+    	case 15: return this.leafShread; 
+    	case 16: return this.leafMalf; 
+    	case 17: return this.leafMild; 
+    	case 18: return this.stem; 
+    	case 19: return this.lodging; 
+    	case 20: return this.stemCankers; 
+    	case 21: return this.cankerLesion;
+    	case 22: return this.fruitingBodies;
+    	case 23: return this.externalDecay;
+    	case 24: return this.mycelium; 
+    	case 25: return this.intDiscolor;
+    	case 26:	return this.sclerotia; 
+    	case 27: return this.fruitPods; 
+    	case 28: return this.fruitSpots; 
+    	case 29: return this.seed; 
+    	case 30: return this.moldGrowth; 
+    	case 31: return this.seedDiscolor; 
+    	case 32: return this.seedSize;
+    	case 33: return this.shriveling;
+    	case 34: return this.roots;
+    	case 35:
+    		if(this.data.equals("D1")) {
+    			return 1;
+    		}
+    		else if(this.data.equals("D2")) {
+    			return 2;
+    		}
+    		else if(this.data.equals("D3")) {
+    			return 3;
+    		}
+    		else {
+    			return 4;
+    		}
+    	}
+    	return 1000;
     }
 }
--- CSCI447/src/main.java
+++ CSCI447/src/main.java
@@ -98,10 +98,13 @@ public class main {
 					Integer.parseInt(values[11]), Integer.parseInt(values[12]), Integer.parseInt(values[13]), Integer.parseInt(values[14]), Integer.parseInt(values[15]), Integer.parseInt(values[16]), 
 					Integer.parseInt(values[17]), Integer.parseInt(values[18]), Integer.parseInt(values[19]), Integer.parseInt(values[20]), Integer.parseInt(values[21]), Integer.parseInt(values[22]), 
 					Integer.parseInt(values[23]), Integer.parseInt(values[24]), Integer.parseInt(values[25]), Integer.parseInt(values[26]), Integer.parseInt(values[27]), Integer.parseInt(values[28]), 
-					Integer.parseInt(values[29]), Integer.parseInt(values[30]), Integer.parseInt(values[31]), Integer.parseInt(values[32]), Integer.parseInt(values[33]), Integer.parseInt(values[34]));
+					Integer.parseInt(values[29]), Integer.parseInt(values[30]), Integer.parseInt(values[31]), Integer.parseInt(values[32]), Integer.parseInt(values[33]), Integer.parseInt(values[34]), values[35]);
 			soyBeanData.add(newObservation);
 			//newObservation.printSoyBean();
 		}
+		
+		int[] values1= {0,1,2,1,0,3,1,1,0,2,1,1,0,2,2,0,0,0,1,0,1,2,0,0,0,0,0,3,4,0,0,0,0,0,1};
+		soyBean(soyBeanData, values1);
 		
 		File file5 = new File("./breast-cancer-wisconsin.data");
 		Scanner scanner5 = new Scanner(file5);
@@ -115,8 +118,8 @@ public class main {
 				//newObservation.printBreastCancer();
 			}
 	    }
-		int[] values= {1, 1, 1, 1, 1, 1, 1, 1, 1};
-		breastCancer(breastCancerData, values);
+		int[] values= {5, 1, 1, 7, 1, 1, 9, 4, 10};
+		//breastCancer(breastCancerData, values);
 	}
 
 /////////////////////////////////////////////Pre processing/////////////////////////////////////////////////////////////////////////////////////////
@@ -453,7 +456,287 @@ public class main {
 			System.out.println("Benign and Malignant have the same probability");
 		}
 }
+	
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////Soy Bean//////////////////////////////////////////////////////////////
+	public static void soyBean(ArrayList<SoyBean> data, int[] values) {
+		int[] totals=new int[4]; //0 for 1, 1 for 2, 2 for 3, 3 for 4 
+		
+		//For D1 
+		int[] date= new int[28]; //0-6 for 1, 7-13 for 2, 14-20 for 3, 21-27 for 4
+		int[] plantStand= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4
+		int[] precip= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] temp= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;;
+		int[] hail= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] cropHist= new int[16]; //0-3 for 1, 4-7 for 2, 8-11 for 3, 12-5 for 4;
+		int[] areaDamage= new int[16]; //0-3 for 1, 4-7 for 2, 8-11 for 3, 12-5 for 4;
+		int[] severity= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] seedTmt= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] germination= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;;
+		int[] plantGrowth= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] leaves= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] leafspotsHalo= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] leafspotsMarg= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] leafspotsSize= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] leafShread= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] leafMalf= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] leafMild= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] stem= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] lodging= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[]stemCankers= new int[16]; //0-3 for 1, 4-7 for 2, 8-11 for 3, 12-5 for 4;
+		int[] cankerLesion= new int[16]; //0-3 for 1, 4-7 for 2, 8-11 for 3, 12-5 for 4;
+		int[] fruitingBodies= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] externalDecay= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] mycelium= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] intDiscolor= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
+		int[] sclerotia= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] fruitPods= new int[16]; //0-3 for 1, 4-7 for 2, 8-11 for 3, 12-5 for 4;
+		int[] fruitSpots= new int[20]; //0-4 for 1, 5-9 for 2, 10-14 for 3, 15-19 for 4;
+		int[] seed= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] moldGrowth= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] seedDiscolor= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] seedSize= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] shriveling= new int[8]; //0-1 for 1, 2-3 for 2, 4-5 for 3, 6-7 for 4;
+		int[] roots= new int[12]; //0-2 for 1, 3-5 for 2, 6-8 for 3, 9-11 for 4;
 
+	
+		// Counter
+		for(int i = 0; i < data.size(); i++) {// Rows
+			for(int j=0; j<36; j++) {// Columns
+				if(data.get(i).getColumn(35)==1) {
+					switch(j) {
+			    	case 0: date[data.get(i).getColumn(j)]++; break;
+			    	case 1: plantStand[data.get(i).getColumn(j)]++; break;
+			    	case 2: precip[data.get(i).getColumn(j)]++;  break;
+			    	case 3: temp[data.get(i).getColumn(j)]++; break;
+			    	case 4:	hail[data.get(i).getColumn(j)]++;  break;
+			    	case 5: cropHist[data.get(i).getColumn(j)]++;  break;
+			    	case 6: areaDamage[data.get(i).getColumn(j)]++;  break;
+			    	case 7: severity[data.get(i).getColumn(j)]++;  break;
+			    	case 8: seedTmt[data.get(i).getColumn(j)]++;  break;
+			    	case 9: germination[data.get(i).getColumn(j)]++;  break;
+			    	case 10: plantGrowth[data.get(i).getColumn(j)]++; break;
+			    	case 11: leaves[data.get(i).getColumn(j)]++; break;
+			    	case 12: leafspotsHalo[data.get(i).getColumn(j)]++; break;
+			    	case 13: leafspotsMarg[data.get(i).getColumn(j)]++;  break;
+			    	case 14: leafspotsSize[data.get(i).getColumn(j)]++; break;
+			    	case 15: leafShread[data.get(i).getColumn(j)]++;  break;
+			    	case 16: leafMalf[data.get(i).getColumn(j)]++;  break;
+			    	case 17: leafMild[data.get(i).getColumn(j)]++;  break;
+			    	case 18: stem[data.get(i).getColumn(j)]++;  break;
+			    	case 19: lodging[data.get(i).getColumn(j)]++;  break;
+			    	case 20: stemCankers[data.get(i).getColumn(j)]++;  break;
+			    	case 21: cankerLesion[data.get(i).getColumn(j)]++; break;
+			    	case 22: fruitingBodies[data.get(i).getColumn(j)]++; break;
+			    	case 23: externalDecay[data.get(i).getColumn(j)]++; break;
+			    	case 24: mycelium[data.get(i).getColumn(j)]++;  break;
+			    	case 25: intDiscolor[data.get(i).getColumn(j)]++; break;
+			    	case 26: sclerotia[data.get(i).getColumn(j)]++;  break;
+			    	case 27: fruitPods[data.get(i).getColumn(j)]++;  break;
+			    	case 28: fruitSpots[data.get(i).getColumn(j)]++;  break;
+			    	case 29: seed[data.get(i).getColumn(j)]++;  break;
+			    	case 30: moldGrowth[data.get(i).getColumn(j)]++;  break;
+			    	case 31: seedDiscolor[data.get(i).getColumn(j)]++;  break;
+			    	case 32: seedSize[data.get(i).getColumn(j)]++; break;
+			    	case 33: shriveling[data.get(i).getColumn(j)]++; break;
+			    	case 34: roots[data.get(i).getColumn(j)]++; break;
+			    	case 35: totals[0]++; break;
+			    	}
+				}
+				else if(data.get(i).getColumn(35)==2) {
+					switch(j) {
+			    	case 0: date[7+data.get(i).getColumn(j)]++; break;
+			    	case 1: plantStand[2+data.get(i).getColumn(j)]++; break;
+			    	case 2: precip[3+data.get(i).getColumn(j)]++;  break;
+			    	case 3: temp[3+data.get(i).getColumn(j)]++; break;
+			    	case 4:	hail[2+data.get(i).getColumn(j)]++;  break;
+			    	case 5: cropHist[4+data.get(i).getColumn(j)]++;  break;
+			    	case 6: areaDamage[4+data.get(i).getColumn(j)]++;  break;
+			    	case 7: severity[3+data.get(i).getColumn(j)]++;  break;
+			    	case 8: seedTmt[3+data.get(i).getColumn(j)]++;  break;
+			    	case 9: germination[3+data.get(i).getColumn(j)]++;  break;
+			    	case 10: plantGrowth[2+data.get(i).getColumn(j)]++; break;
+			    	case 11: leaves[2+data.get(i).getColumn(j)]++; break;
+			    	case 12: leafspotsHalo[3+data.get(i).getColumn(j)]++; break;
+			    	case 13: leafspotsMarg[3+data.get(i).getColumn(j)]++;  break;
+			    	case 14: leafspotsSize[3+data.get(i).getColumn(j)]++; break;
+			    	case 15: leafShread[2+data.get(i).getColumn(j)]++;  break;
+			    	case 16: leafMalf[2+data.get(i).getColumn(j)]++;  break;
+			    	case 17: leafMild[3+data.get(i).getColumn(j)]++;  break;
+			    	case 18: stem[2+data.get(i).getColumn(j)]++;  break;
+			    	case 19: lodging[2+data.get(i).getColumn(j)]++;  break;
+			    	case 20: stemCankers[4+data.get(i).getColumn(j)]++;  break;
+			    	case 21: cankerLesion[4+data.get(i).getColumn(j)]++; break;
+			    	case 22: fruitingBodies[2+data.get(i).getColumn(j)]++; break;
+			    	case 23: externalDecay[3+data.get(i).getColumn(j)]++; break;
+			    	case 24: mycelium[2+data.get(i).getColumn(j)]++;  break;
+			    	case 25: intDiscolor[3+data.get(i).getColumn(j)]++; break;
+			    	case 26: sclerotia[2+data.get(i).getColumn(j)]++;  break;
+			    	case 27: fruitPods[4+data.get(i).getColumn(j)]++;  break;
+			    	case 28: fruitSpots[5+data.get(i).getColumn(j)]++;  break;
+			    	case 29: seed[2+data.get(i).getColumn(j)]++;  break;
+			    	case 30: moldGrowth[2+data.get(i).getColumn(j)]++;  break;
+			    	case 31: seedDiscolor[2+data.get(i).getColumn(j)]++;  break;
+			    	case 32: seedSize[2+data.get(i).getColumn(j)]++; break;
+			    	case 33: shriveling[2+data.get(i).getColumn(j)]++; break;
+			    	case 34: roots[3+data.get(i).getColumn(j)]++; break;
+			    	case 35: totals[1]++; break;
+			    	}
+				}
+				else if(data.get(i).getColumn(35)==3) {
+					switch(j) {
+			    	case 0: date[14+data.get(i).getColumn(j)]++; break;
+			    	case 1: plantStand[4+data.get(i).getColumn(j)]++; break;
+			    	case 2: precip[6+data.get(i).getColumn(j)]++;  break;
+			    	case 3: temp[6+data.get(i).getColumn(j)]++; break;
+			    	case 4:	hail[4+data.get(i).getColumn(j)]++;  break;
+			    	case 5: cropHist[8+data.get(i).getColumn(j)]++;  break;
+			    	case 6: areaDamage[8+data.get(i).getColumn(j)]++;  break;
+			    	case 7: severity[6+data.get(i).getColumn(j)]++;  break;
+			    	case 8: seedTmt[6+data.get(i).getColumn(j)]++;  break;
+			    	case 9: germination[6+data.get(i).getColumn(j)]++;  break;
+			    	case 10: plantGrowth[4+data.get(i).getColumn(j)]++; break;
+			    	case 11: leaves[4+data.get(i).getColumn(j)]++; break;
+			    	case 12: leafspotsHalo[6+data.get(i).getColumn(j)]++; break;
+			    	case 13: leafspotsMarg[6+data.get(i).getColumn(j)]++;  break;
+			    	case 14: leafspotsSize[6+data.get(i).getColumn(j)]++; break;
+			    	case 15: leafShread[4+data.get(i).getColumn(j)]++;  break;
+			    	case 16: leafMalf[4+data.get(i).getColumn(j)]++;  break;
+			    	case 17: leafMild[6+data.get(i).getColumn(j)]++;  break;
+			    	case 18: stem[4+data.get(i).getColumn(j)]++;  break;
+			    	case 19: lodging[4+data.get(i).getColumn(j)]++;  break;
+			    	case 20: stemCankers[8+data.get(i).getColumn(j)]++;  break;
+			    	case 21: cankerLesion[8+data.get(i).getColumn(j)]++; break;
+			    	case 22: fruitingBodies[4+data.get(i).getColumn(j)]++; break;
+			    	case 23: externalDecay[6+data.get(i).getColumn(j)]++; break;
+			    	case 24: mycelium[4+data.get(i).getColumn(j)]++;  break;
+			    	case 25: intDiscolor[6+data.get(i).getColumn(j)]++; break;
+			    	case 26: sclerotia[4+data.get(i).getColumn(j)]++;  break;
+			    	case 27: fruitPods[8+data.get(i).getColumn(j)]++;  break;
+			    	case 28: fruitSpots[10+data.get(i).getColumn(j)]++;  break;
+			    	case 29: seed[4+data.get(i).getColumn(j)]++;  break;
+			    	case 30: moldGrowth[4+data.get(i).getColumn(j)]++;  break;
+			    	case 31: seedDiscolor[4+data.get(i).getColumn(j)]++;  break;
+			    	case 32: seedSize[4+data.get(i).getColumn(j)]++; break;
+			    	case 33: shriveling[4+data.get(i).getColumn(j)]++; break;
+			    	case 34: roots[6+data.get(i).getColumn(j)]++; break;
+			    	case 35: totals[2]++; break;
+			    	}
+				}
+				else  {
+					switch(j) {
+			    	case 0: date[21+data.get(i).getColumn(j)]++; break;
+			    	case 1: plantStand[6+data.get(i).getColumn(j)]++; break;
+			    	case 2: precip[9+data.get(i).getColumn(j)]++;  break;
+			    	case 3: temp[9+data.get(i).getColumn(j)]++; break;
+			    	case 4:	hail[6+data.get(i).getColumn(j)]++;  break;
+			    	case 5: cropHist[12+data.get(i).getColumn(j)]++;  break;
+			    	case 6: areaDamage[12+data.get(i).getColumn(j)]++;  break;
+			    	case 7: severity[9+data.get(i).getColumn(j)]++;  break;
+			    	case 8: seedTmt[9+data.get(i).getColumn(j)]++;  break;
+			    	case 9: germination[9+data.get(i).getColumn(j)]++;  break;
+			    	case 10: plantGrowth[6+data.get(i).getColumn(j)]++; break;
+			    	case 11: leaves[6+data.get(i).getColumn(j)]++; break;
+			    	case 12: leafspotsHalo[9+data.get(i).getColumn(j)]++; break;
+			    	case 13: leafspotsMarg[9+data.get(i).getColumn(j)]++;  break;
+			    	case 14: leafspotsSize[9+data.get(i).getColumn(j)]++; break;
+			    	case 15: leafShread[6+data.get(i).getColumn(j)]++;  break;
+			    	case 16: leafMalf[6+data.get(i).getColumn(j)]++;  break;
+			    	case 17: leafMild[9+data.get(i).getColumn(j)]++;  break;
+			    	case 18: stem[6+data.get(i).getColumn(j)]++;  break;
+			    	case 19: lodging[6+data.get(i).getColumn(j)]++;  break;
+			    	case 20: stemCankers[12+data.get(i).getColumn(j)]++;  break;
+			    	case 21: cankerLesion[12+data.get(i).getColumn(j)]++; break;
+			    	case 22: fruitingBodies[6+data.get(i).getColumn(j)]++; break;
+			    	case 23: externalDecay[9+data.get(i).getColumn(j)]++; break;
+			    	case 24: mycelium[6+data.get(i).getColumn(j)]++;  break;
+			    	case 25: intDiscolor[9+data.get(i).getColumn(j)]++; break;
+			    	case 26: sclerotia[6+data.get(i).getColumn(j)]++;  break;
+			    	case 27: fruitPods[12+data.get(i).getColumn(j)]++;  break;
+			    	case 28: fruitSpots[15+data.get(i).getColumn(j)]++;  break;
+			    	case 29: seed[6+data.get(i).getColumn(j)]++;  break;
+			    	case 30: moldGrowth[6+data.get(i).getColumn(j)]++;  break;
+			    	case 31: seedDiscolor[6+data.get(i).getColumn(j)]++;  break;
+			    	case 32: seedSize[6+data.get(i).getColumn(j)]++; break;
+			    	case 33: shriveling[6+data.get(i).getColumn(j)]++; break;
+			    	case 34: roots[9+data.get(i).getColumn(j)]++; break;
+			    	case 35: totals[3]++; break;
+			    	}
+				}
+			}	
+		}
+		int totalValues=totals[0]+totals[1]+totals[2]+totals[3];
+		float pD1= (float) totals[0]/totalValues;
+		float pD2= (float) totals[1]/totalValues;
+		float pD3= (float) totals[2]/totalValues;
+		float pD4= (float) totals[3]/totalValues;
+		float[] pDi= {pD1, pD2, pD3, pD4};
+		float[] probability= new float [140]; //0-34 for 1, 35-69 for 2, 70-104 for 3, 105-139 for 4 
+		float[] finalProbability= new float[4]; //0 for 1, 1 for 2, 2 for 3, 3 for 4 
+		
+		for (int i=0; i<4; i++) {
+			probability[35*i+0]=(float) (date[7*i+values[0]])/totals[i];
+			probability[35*i+1]=(float) (plantStand[2*i+values[1]])/totals[i];
+			probability[35*i+2]=(float) (precip[3*i+values[2]])/totals[i];
+			probability[35*i+3]=(float) (temp[3*i+values[3]])/totals[i];
+			probability[35*i+4]=(float) (hail[2*i+values[4]])/totals[i];
+			probability[35*i+5]=(float) (cropHist[4*i+values[5]])/totals[i];
+			probability[35*i+6]=(float) (areaDamage[4*i+values[6]])/totals[i];
+			probability[35*i+7]=(float) (severity[3*i+values[7]])/totals[i];
+			probability[35*i+8]=(float) (seedTmt[3*i+values[8]])/totals[i];
+			probability[35*i+9]=(float) (germination[3*i+values[9]])/totals[i];
+			probability[35*i+10]=(float) (plantGrowth[2*i+values[10]])/totals[i];
+			probability[35*i+11]=(float) (leaves[2*i+values[11]])/totals[i];
+			probability[35*i+12]=(float) (leafspotsHalo[3*i+values[12]])/totals[i];
+			probability[35*i+13]=(float) (leafspotsMarg[3*i+values[13]])/totals[i];
+			probability[35*i+14]=(float) (leafspotsSize[3*i+values[14]])/totals[i];
+			probability[35*i+15]=(float) (leafShread[2*i+values[15]])/totals[i];
+			probability[35*i+16]=(float) (leafMalf[2*i+values[16]])/totals[i];
+			probability[35*i+17]=(float) (leafMild[3*i+values[17]])/totals[i];
+			probability[35*i+18]=(float) (stem[2*i+values[18]])/totals[i];
+			probability[35*i+19]=(float) (lodging[2*i+values[19]])/totals[i];
+			probability[35*i+20]=(float) (stemCankers[4*i+values[20]])/totals[i];
+			probability[35*i+21]=(float) (cankerLesion[4*i+values[21]])/totals[i];
+			probability[35*i+22]=(float) (fruitingBodies[2*i+values[22]])/totals[i];
+			probability[35*i+23]=(float) (externalDecay[3*i+values[23]])/totals[i];
+			probability[35*i+24]=(float) (mycelium[2*i+values[24]])/totals[i];
+			probability[35*i+25]=(float) (intDiscolor[3*i+values[25]])/totals[i];
+			probability[35*i+26]=(float) (sclerotia[2*i+values[26]])/totals[i];
+			probability[35*i+27]=(float) (fruitPods[4*i+values[27]])/totals[i];
+			probability[35*i+28]=(float) (fruitSpots[5*i+values[28]])/totals[i];
+			probability[35*i+29]=(float) (seed[2*i+values[29]])/totals[i];
+			probability[35*i+30]=(float) (moldGrowth[2*i+values[30]])/totals[i];
+			probability[35*i+31]=(float) (seedDiscolor[2*i+values[31]])/totals[i];
+			probability[35*i+32]=(float) (seedSize[2*i+values[32]])/totals[i];
+			probability[35*i+33]=(float) (shriveling[2*i+values[33]])/totals[i];
+			probability[35*i+34]=(float) (roots[3*i+values[34]])/totals[i];
+			
+			finalProbability[i]=(float) probability[35*i+0]*probability[35*i+1]*probability[35*i+2]*probability[35*i+3]*probability[35*i+4]*probability[35*i+5]
+					*probability[35*i+6]*probability[35*i+7]*probability[35*i+8]*probability[35*i+9]*probability[35*i+10]*probability[35*i+11]*probability[35*i+12]
+					*probability[35*i+13]*probability[35*i+14]*probability[35*i+15]*probability[35*i+16]*probability[35*i+17]*probability[35*i+18]*probability[35*i+19]
+					*probability[35*i+20]*probability[35*i+21]*probability[35*i+22]*probability[35*i+23]*probability[35*i+24]*probability[35*i+25]*probability[35*i+26]
+					*probability[35*i+27]*probability[35*i+28]*probability[35*i+29]*probability[35*i+30]*probability[35*i+31]*probability[35*i+32]*probability[35*i+33]
+					*probability[35*i+34]*pDi[i];
+		}
+		System.out.println(finalProbability[0]);
+		System.out.println(finalProbability[1]);
+		System.out.println(finalProbability[2]);
+		System.out.println(finalProbability[3]);
+		if (finalProbability[0]>finalProbability[1] && finalProbability[0]>finalProbability[2] && finalProbability[0]>finalProbability[3]) {
+			System.out.println("D1 has the biggest probability");
+		}
+		else if (finalProbability[1]>finalProbability[0] && finalProbability[1]>finalProbability[2] && finalProbability[1]>finalProbability[3]) {
+			System.out.println("D2 has the biggest probability");
+		}
+		else if (finalProbability[2]>finalProbability[1] && finalProbability[2]>finalProbability[0] && finalProbability[2]>finalProbability[3]) {
+			System.out.println("D3 has the biggest probability");
+		}
+		else if (finalProbability[3]>finalProbability[1] && finalProbability[3]>finalProbability[2] && finalProbability[3]>finalProbability[0]) {
+			System.out.println("D4 has the biggest probability");
+		}
+		
+			
+}
 	///////////////////////////////////10%/////////////////////////////////////////////////////////////////
 	//shuffles data in one column of HouseVotes
 		public static ArrayList<HouseVotes> houseVotesShuffle(ArrayList<HouseVotes> data, int column) {
